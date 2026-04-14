d8e52e397f14f3a587bb28f579a0dc65d03139e1
Кабаков Иван <i.kabakov@sibstrin.ru>
2026-04-14 23:40:12 +0700

    Add files via upload

---
+++ gitattributes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Numerics;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для отображения языка в репозитории");
+        }
+    }
+}
